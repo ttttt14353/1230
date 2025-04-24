d1f53beb9eb501163572fe569b50c3fdd27c3559
taka。 <taka@example.com>
2025-04-25 08:09:39 +0900

    Final fix and sync

---
+++ static/js/menu.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
+  
+    toggle.addEventListener("click", () => {
+      menu.classList.toggle("show");
+    });
+  });
